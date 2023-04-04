fdfe8312bb657d495900ecb856a9b5533005b385
barbosawf <wagner.barbosa@ufv.br>
2023-04-04 20:59:28 -0300

    Allele frequency Changes in sub_gen function

--- codification_for_lineage.R
+++ codification_for_lineage.R
@@ -1,8 +1,20 @@
+
+# Pacotes necessários -----------------------------------------------------
+
 library(tidyverse)
+library(plyr)
+
+
+# Banco de dados ----------------------------------------------------------
 
 d <- read.table('subset.txt', header = T)
 
 d <- as_tibble(t(d))
+
+d
+
+
+# Transformação dos genótipos (Heterozigoto recebe NA) --------------------
 
 sub_gen <- function(x) {
   x |>
@@ -11,8 +23,10 @@ sub_gen <- function(x) {
   order_freq_table  <- freq_table[order(freq_table$freq, decreasing = T), ]
   
   gen_major_freq <- order_freq_table[1, 1]
+  gen_minor_freq <- order_freq_table[2, 1]
   
-  ifelse(x == gen_major_freq, 1, 0)
+ ifelse(x == gen_major_freq, 2, ifelse(x == gen_minor_freq, 0, NA))
+
 }
 
 coded_d1 <- apply(d, 2, sub_gen)
@@ -20,4 +34,53 @@ coded_d1 <- apply(d, 2, sub_gen)
 
 coded_d2 <- map_df(d, sub_gen)
 
+
+
+# Frequência dos allelos em cada SNP --------------------------------------
+
+freq_alle <- function(x) {
+  
+  if (sum(is.na(x)) == length(x)) {
+    out <- c(Alleles = "NA",
+             Frequency = '1')
+    
+  } else {
+    
+    x |>
+      na.omit() |>
+      strsplit(NULL) |>
+      unlist() |>
+      plyr::count() -> freq_table
+    
+    r <- freq_table$freq / sum(freq_table$freq)
+    
+    freq_table$rate <- format(round(r, 2), nsmall = 2)
+    
+    order_freq_table  <-
+      freq_table[order(freq_table$freq, decreasing = T),]
+    
+    a_max <- order_freq_table[1, 1]
+    a_min <- order_freq_table[2, 1]
+    
+    r_max <- order_freq_table[1, 3]
+    r_min <- order_freq_table[2, 3]
+    
+    if (r_max == '1.00')  {
+      out <- c(Alleles = a_max,
+               Frequency = r_max)
+    } else {
+      out <- c(
+        Alleles = paste0(a_max, '/', a_min),
+        Frequency = paste0(r_max, '/', r_min)
+      )
+    }
+    
+  }
+  
+  out
+  
+}
+
+
+map_dfr(d, freq_alle, .id = 'SNPs')
 
